ef9a16d073d81f812084b0d5dc38ad620b89ac2a
ameenalmassih <a.almassih@gmail.com>
2018-09-13 15:57:13 -0400

    test

--- app/src/main/java/com/example/ameenalmassih/ticketingapplication/MainPage.java
+++ app/src/main/java/com/example/ameenalmassih/ticketingapplication/MainPage.java
@@ -1,14 +1,25 @@
 package com.example.ameenalmassih.ticketingapplication;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private static final String tag = "MainActivity";
+    private TextView mDisplayDate;
+    private DatePickerDialog.OnDateSetListener mdateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +31,29 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+
+        mDisplayDate = (TextView) findViewById(R.Id.tvDate);
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MainPage.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        mdateSetListener, year, month, day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
     }
+
+    
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
