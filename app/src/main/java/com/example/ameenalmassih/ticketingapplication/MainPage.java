2a22f78b82e0c2c0ffe90a71d54cbf924d3fce72
ameenalmassih <a.almassih@gmail.com>
2018-09-14 15:01:09 -0400

    test

--- app/src/main/java/com/example/ameenalmassih/ticketingapplication/MainPage.java
+++ app/src/main/java/com/example/ameenalmassih/ticketingapplication/MainPage.java
@@ -19,7 +19,7 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
 
     private static final String tag = "MainActivity";
     private TextView mDisplayDate;
-    private DatePickerDialog.OnDateSetListener mdateSetListener;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
 
     @Override
@@ -33,6 +33,12 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
+        Spinner spinner2 = findViewById(R.id.cities);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.cities, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
 
         mDisplayDate = (TextView) findViewById(R.id.tvDate);
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
@@ -45,14 +51,14 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
 
                 DatePickerDialog dialog = new DatePickerDialog(MainPage.this,
                         android.R.style.Theme_Holo_Dialog_MinWidth,
-                        mdateSetListener, year, month, day);
+                        mDateSetListener, year, month, day);
 
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
 
-        mdateSetListener = new DatePickerDialog.OnDateSetListener() {
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month + 1;
@@ -68,6 +74,8 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
         String text = parent.getItemAtPosition(position).toString();
         Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
     }
+
+
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
